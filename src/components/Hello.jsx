14f1d40d063129876e3ebca0a3dd2404de93dc01
Lucas Becerra <lucaseramos13@gmail.com>
2023-07-03 22:14:23 -0300

    add social buttons and style

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, Typography } from '@mui/joy';
 import Typewriter from 'typewriter-effect'
 import { keyframes } from '@emotion/react';
+import './Hello.css'
 
 const gradient = keyframes`
 0% {
@@ -21,15 +22,34 @@ new Typewriter('#typewriter', {
 const Hello = ( ) => {
   return (
     <Box id='hello'>
+     
     <Box sx={{display:'flex', justifyContent:'center', alignItems: 'center', flexDirection: 'column', marginTop:'5rem'}}>
       <Typography level='h4'> Hi 👋🏻, I am</Typography>
       <Typography level='h1' sx={{fontWeight:'700', fontSize:'8rem', lineHeight:"7rem", background:"-webkit-linear-gradient(-45deg,#ee7752,#e73c7e,#23a6d5, #23d5ab)",backgroundSize:"250%", WebkitBackgroundClip:"text", WebkitTextFillColor:"transparent", animation:`${gradient} 5s ease infinite`, display:'flex', flexDirection:'column', alignItems:'center'}}>Lucas <span>Ramos.</span></Typography>
       <Typography level='h3' sx={{marginTop:'2rem'}}>Fronted Developer.</Typography>
     </Box>
     <Box sx={{display:'flex', justifyContent:'center', gap:'1rem', marginTop:'1rem'}}>
-    <Button size='lg' sx={{ backgroundColor:'#87ceeb', color:'#214949', ":hover":{backgroundColor:'#e4edf1 '}}}  >Download cv</Button>
-    <Button size='lg' sx={{ backgroundColor:'#87ceeb', color:'#214949', ":hover":{backgroundColor:'#e4edf1 '} }} >Contact me</Button>
+    <Button size='lg' sx={{ backgroundColor:'#0c4489', color:'#e4edf1', ":hover":{scale:'1.1'}}}  >Download cv</Button>
+    <Button size='lg' sx={{ backgroundColor:'#0c4489', color:'#e4edf1', ":hover":{scale:'1.1'} }} >Contact me</Button>
     </Box>
+    <ul className='social-buttons-list'>
+        <li className='item-social'>
+          <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>
+          </a>
+          </li>
+        <li className='item-social'>
+          <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><rect x="4" y="4" width="16" height="16" rx="2"></rect><line x1="8" y1="11" x2="8" y2="16"></line><line x1="8" y1="8" x2="8" y2="8.01"></line><line x1="12" y1="16" x2="12" y2="11"></line><path d="M16 16v-3a2 2 0 0 0 -4 0"></path></svg>
+          </a>
+          </li>
+        <li className='item-social'>
+          <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><rect x="3" y="5" width="18" height="14" rx="2"></rect><polyline points="3 7 12 13 21 7"></polyline></svg>
+          </a>
+          </li>
+        <div className='social-button-div'></div>
+      </ul>
     </Box>
   )
 }
