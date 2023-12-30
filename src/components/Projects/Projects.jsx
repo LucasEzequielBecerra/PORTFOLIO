15947695168d6a770742076ccea155018127500e
Lucas Becerra <lucaseramos13@gmail.com>
2023-12-30 17:15:10 -0300

    fix url twc

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,8 +17,8 @@ const Projects = () => {
               <li>Tailwind</li>
               <li>JavaScript</li>
             </ul>
-            <div className='page-info'><a target='_blank' href="https://github.com/LucasEzequielRamos/The-Way-Camp" rel="noreferrer"><svg className='icon-info' xmlns="http://www.w3.org/2000/svg" stroke='currentColor' strokeWidth="2" fill='none' viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
-              <a target='_blank' href="the-way-camp-3vqwhjepx-lucasezequielbecerra.vercel.app" rel="noreferrer">
+            <div className='page-info'><a target='_blank' href="github.com/LucasEzequielRamos/The-Way-Camp" rel="noreferrer"><svg className='icon-info' xmlns="http://www.w3.org/2000/svg" stroke='currentColor' strokeWidth="2" fill='none' viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
+              <a target='_blank' href="https://the-way-camp-3vqwhjepx-lucasezequielbecerra.vercel.app" rel="noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" className='icon-info' viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="9"></circle><line x1="3.6" y1="9" x2="20.4" y2="9"></line><line x1="3.6" y1="15" x2="20.4" y2="15"></line><path d="M11.5 3a17 17 0 0 0 0 18"></path><path d="M12.5 3a17 17 0 0 1 0 18"></path></svg>
               </a></div>
           </div>
