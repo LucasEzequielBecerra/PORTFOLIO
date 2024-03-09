00502fc61ab06580e8adcc94c301df4987f2eec8
Lucas Becerra <lucaseramos13@gmail.com>
2024-03-09 01:08:58 -0300

    add new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -43,20 +43,20 @@ const Projects = () => {
           </div>
         </article>
         <article className='single-project'>
-          <img className='project-image' src="E-Commerce-API-project.png" alt="E-Commerce-API Background Image" />
+          <img className='project-image' src="Twitter-Clone-project.jpg" alt="Twitter Clone Background Image" />
           <div className='info-column'>
-            <h3 className='title-single-project' >E-Commerce-API</h3>
-            <h6 className='project-description'>This is my final backend project to finish my degree as a full stack programmer in CoderHouse. This project is a e-commerce API for add and manage users and products</h6>
+            <h3 className='title-single-project' >Twitter Clone</h3>
+            <h6 className='project-description'>This project is a clone of the social network Twitter with some of its functionalities. I use the technologies of NextJS, Prisma ORM, MySQL, Tailwind. Implemented use of Next Auth to handle authorization and session cookies. I implement server actions with fetch to my api routes, with header management and custom server responses.</h6>
             <ul className='skills-applied'>
-              <li>Node</li>
-              <li>MongoDB</li>
-              <li>Express</li>
-              <li>Passport</li>
-              <li>Swagger</li>
-              <li>Express-Session</li>
+              <li>NextJS</li>
+              <li>Typescript</li>
+              <li>Prisma ORM</li>
+              <li>MySQL</li>
+              <li>Tailwind</li>
+              <li>Next Auth</li>
             </ul>
-            <div className='page-info'><a target='_blank' href="https://github.com/LucasEzequielRamos/Ecommerce-Api" rel="noreferrer"><svg className='icon-info' xmlns="http://www.w3.org/2000/svg" stroke='currentColor' strokeWidth="2" fill='none' viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
-              <a target='_blank' href="https://ecommerce-api-dev-txxg.1.us-1.fl0.io/docs/" rel="noreferrer">
+            <div className='page-info'><a target='_blank' href="https://github.com/LucasEzequielRamos/Twitter-Clone" rel="noreferrer"><svg className='icon-info' xmlns="http://www.w3.org/2000/svg" stroke='currentColor' strokeWidth="2" fill='none' viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
+              <a target='_blank' href="https://twitter-clone-theta-bay.vercel.app/" rel="noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" className='icon-info' viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="9"></circle><line x1="3.6" y1="9" x2="20.4" y2="9"></line><line x1="3.6" y1="15" x2="20.4" y2="15"></line><path d="M11.5 3a17 17 0 0 0 0 18"></path><path d="M12.5 3a17 17 0 0 1 0 18"></path></svg>
               </a></div>
           </div>
@@ -81,17 +81,18 @@ const Projects = () => {
               </a></div>
           </article>
           <article className='other-projects-card'>
-            <h4 className='title-other-project'>Amazing-Movie-Searcher</h4>
-            <p> This project was taken from a technical test for a junior or front-end trainee using react, handling requests to the moviedb API. Implementing the use of hooks and search and filtering logic</p>
+            <h4 className='title-other-project'>E-Commerce-API</h4>
+            <p> This is my final backend project to finish my degree as a full stack programmer in CoderHouse. This project is a e-commerce API for add and manage users and products</p>
             <ul className='skills-applied-min'>
-              <li>Html</li>
-              <li>Css</li>
-              <li>ReactJs</li>
-              <li>Just-debounce-it</li>
-              <li>Vite</li>
+              <li>Node</li>
+              <li>MongoDB</li>
+              <li>Express</li>
+              <li>Passport</li>
+              <li>Swagger</li>
+              <li>Express-Session</li>
             </ul>
-            <div className='links-container'><a target='_blank' href="https://github.com/LucasEzequielBecerra/Amazing-Movie-Searcher" rel="noreferrer"><svg className='icon-info' xmlns="http://www.w3.org/2000/svg" stroke='currentColor' strokeWidth="2" fill='none' viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
-              <a target='_blank' href="https://movie-searcher-app-two.vercel.app/" rel="noreferrer">
+            <div className='links-container'><a target='_blank' href="https://github.com/LucasEzequielRamos/Ecommerce-Api" rel="noreferrer"><svg className='icon-info' xmlns="http://www.w3.org/2000/svg" stroke='currentColor' strokeWidth="2" fill='none' viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
+              <a target='_blank' href="https://ecommerce-api-dev-txxg.1.us-1.fl0.io/docs/" rel="noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" className='icon-info' viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="9"></circle><line x1="3.6" y1="9" x2="20.4" y2="9"></line><line x1="3.6" y1="15" x2="20.4" y2="15"></line><path d="M11.5 3a17 17 0 0 0 0 18"></path><path d="M12.5 3a17 17 0 0 1 0 18"></path></svg>
               </a></div>
           </article>
